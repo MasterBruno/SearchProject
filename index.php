efb06887183c5f6698c7506c608a2755b99797b8
Unknown <brunoc.15@hotmail.com>
2018-12-28 14:38:14 -0200

    Layout Redefinido

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<html>
+<html lang="pt_BR">
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -6,6 +6,7 @@
     <meta name="author" content="colorlib.com">
     <link href="https://fonts.googleapis.com/css?family=Poppins:400,800" rel="stylesheet" />
     <link href="css/main.css" rel="stylesheet" />
+    <title>Search</title>
 
     <!-- Bootstrap core CSS-->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -22,68 +23,130 @@
   </head>
   <body>
     <div class="s006">
-      <form>
+      <form action="<?php $_SERVER['PHP_SELF'];?>" method="POST">
         <fieldset>
           <legend>O que deseja procurar?</legend>
-          <div class="inner-form">
-            <div class="input-field">
-              <button class="btn-search" type="button">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                  <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
-                </svg>
-              </button>
-              <input id="search" type="text" placeholder="Digite Aqui..." value="" />
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <nav>
+                  <div class="nav nav-tabs justify-content-end" id="nav-tab" role="tablist">
+                    <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-Motces" role="tab" aria-controls="nav-home" aria-selected="true">Motivo de Cessação</a>
+                    <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-Motsus" role="tab" aria-controls="nav-profile" aria-selected="false">Motivo de Suspensão</a>
+                  </div>
+                </nav>
+                <br>
+                <div class="tab-content" id="nav-tabContent">
+                  <div class="tab-pane fade show active" id="nav-Motces" role="tabpanel" aria-labelledby="nav-home-tab">
+                    <div class="inner-form">
+                      <div class="input-group add-on">
+                        <input class="form-control" placeholder="Digite aqui..." name="search" id="srch-term" type="text">
+                        <div class="input-group-btn">
+                          <button class="btn btn-primary button1" type="submit" name="pesquisar"><i class="fa fa-search col-md-12"></i></button>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="suggestion-wrap">
+                      <span>Código</span>
+                    </div>
+                  </div>
+                  <div class="tab-pane fade" id="nav-Motsus" role="tabpanel" aria-labelledby="nav-profile-tab">
+                    <div class="inner-form">
+                      <div class="inner-form">
+                      <div class="input-group add-on">
+                        <input class="form-control" placeholder="Digite aqui..." name="search" id="srch-term" type="text">
+                        <div class="input-group-btn">
+                          <button class="btn btn-primary button1" type="submit" name="pesquisar"><i class="fa fa-search col-md-12"></i></button>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="suggestion-wrap">
+                      <span>Código</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-          <div class="suggestion-wrap">
-            <span>Código</span>
           </div>
         </fieldset>
       </form>
     </div>
 
-    <!--
-    DataTables Example
-    <div class="card mb-3">
-      <div class="card-header">
-        <i class="fas fa-table"></i>
-        Resultado da Pesquisa</div>
-      <div class="card-body">
-        <div class="table-responsive">
-          <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-            <thead class="thead-dark">
-              <tr>
-                <th>Código</th>
-                <th>Data</th>
-                <th>Descrição da Conta</th>
-                <th>Integrante</th>
-                <th>Valor</th>
-              </tr>
-            </thead>
-            <tfoot>
-              <tr>
-                <th>Código</th>
-                <th>Data</th>
-                <th>Descrição da Conta</th>
-                <th>Integrante</th>
-                <th>Valor</th>
-              </tr>
-            </tfoot>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-                <td>1</td>
-              </tr>
-            </tbody>
-          </table>
+    <?php 
+      if (isset($_POST["pesquisar"])) {
+        $text = $_POST["search"];
+        if(empty($text)) {
+          echo "<script language='javascript' type='text/javascript'>alert('Digite no campo de pesquisa!');</script>";
+        } else {
+          echo "<script language='javascript' type='text/javascript'>alert('Pesquisando...!');</script>";
+    ?>
+
+    <br>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <nav>
+            <div class="nav nav-tabs" id="nav-tab" role="tablist">
+              <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-PRISMA" role="tab" aria-controls="nav-home" aria-selected="true">PRISMA SABI</a>
+              <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-REATNB" role="tab" aria-controls="nav-profile" aria-selected="false">REATNB</a>
+            </div>
+          </nav>
+          <div class="tab-content" id="nav-tabContent">
+            <div class="tab-pane fade show active" id="nav-PRISMA" role="tabpanel" aria-labelledby="nav-home-tab">...</div>
+            <div class="tab-pane fade" id="nav-REATNB" role="tabpanel" aria-labelledby="nav-profile-tab">...</div>
+          </div>
         </div>
       </div>
-      <div class="card-footer small text-muted">Atualizado às '<?php echo date('H:i'); ?>'.</div>
     </div>
-    -->
+
+    <!-- DataTables Example -->
+    <div class="container">
+      <div class="card mb-3">
+        <div class="card-header">
+          <i class="fas fa-table"></i>
+          Resultado da Pesquisa</div>
+        <div class="card-body">
+          <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+              <thead class="thead-dark">
+                <tr>
+                  <th>Código</th>
+                  <th>Data</th>
+                  <th>Descrição da Conta</th>
+                  <th>Integrante</th>
+                  <th>Valor</th>
+                </tr>
+              </thead>
+              <tfoot>
+                <tr>
+                  <th>Código</th>
+                  <th>Data</th>
+                  <th>Descrição da Conta</th>
+                  <th>Integrante</th>
+                  <th>Valor</th>
+                </tr>
+              </tfoot>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>1</td>
+                  <td>1</td>
+                  <td>1</td>
+                  <td>1</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div class="card-footer small text-muted">Atualizado às '<?php echo date('H:i'); ?>'.</div>
+      </div>
+    </div>
+
+    <?php
+        }
+      }
+    ?>
+
 
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
@@ -101,6 +164,10 @@
 
     <!-- Demo scripts for this page-->
     <script src="js/demo/datatables-demo.js"></script>
+
+    <!-- Nav pills -->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
   </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 </html>
